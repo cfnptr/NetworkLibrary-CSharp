c3584577c2c78c30a31adb6d29148376f51e2087
Nikita Fediuchin <thequantumbranch@gmail.com>
2020-01-03 00:32:06 +0200

    Removed old submodule, added unit tests

---
+++ Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace NetworkLibrary
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
